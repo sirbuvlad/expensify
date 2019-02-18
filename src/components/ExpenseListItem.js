76f17ef56468d60af142358eeb5680672f439320
Vlad <sirbuvlad.com@gmail.com>
2019-02-18 15:02:07 +0100

    Setup formatting for amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 
 
@@ -8,8 +9,34 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => {
     return (
     <div>
     <h3><Link to={`/edit/${id}`} >{description}</Link></h3>
-    <p>{amount} - {moment(createdAt).format('MMM Do, YYYY')}</p>
+    <p>
+        {numeral(amount / 100).format('$0,0.00')} 
+        - 
+        {moment(createdAt).format('MMMM Do, YYYY')}</p>
     </div>
 )};
+
+// load a locale
+numeral.register('locale', 'de', {
+    delimiters: {
+        thousands: ' ',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '€'
+    }
+});
+
+// switch between locales
+numeral.locale('de');
 
 export default ExpenseListItem;
